f6b91b37e9cbc5d8459778e6945a65f8d01271a7
sénèque HOUNTONDJI <hountondjisenek@gmail.com>
2023-10-13 11:32:48 +0100

    Mail configuration touched

--- djangoHelpdesk/settings.py
+++ djangoHelpdesk/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -136,7 +139,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hountondjisenek@gmail.com'
-EMAIL_HOST_PASSWORD = 'obalfbwpmfksxcyz'
+EMAIL_HOST_PASSWORD = os.environ.get('APP_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
