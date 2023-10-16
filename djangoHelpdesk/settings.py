39ea1e4ca0a37a774ea72b2dfeb295b56dd835ec
sénèque HOUNTONDJI <hountondjisenek@gmail.com>
2023-10-16 20:54:02 +0100

    .

--- djangoHelpdesk/settings.py
+++ djangoHelpdesk/settings.py
@@ -142,12 +142,15 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+
 ## For vercel deployment###
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'media')
 
 
-MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
