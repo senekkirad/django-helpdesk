c07dd6de7ed433eca452b084ac77f58772bd90e8
sénèque HOUNTONDJI <hountondjisenek@gmail.com>
2023-10-16 16:08:05 +0100

    Somme add for vercel

--- djangoHelpdesk/settings.py
+++ djangoHelpdesk/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-fnr3t-!pygpd3otb3qojxvpedlvv)fa&(25m2+w&s_p*bmjf)!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.versel.app']
 
 AUTH_USER_MODEL = 'user.User'
 
