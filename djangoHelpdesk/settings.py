637312cece8d9555600b009f4f2e2c5f45447874
sénèque HOUNTONDJI <hountondjisenek@gmail.com>
2023-10-16 17:13:08 +0100

    .

--- djangoHelpdesk/settings.py
+++ djangoHelpdesk/settings.py
@@ -129,7 +129,11 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+## For vercel deployment###
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
--- djangoHelpdesk/urls.py
+++ djangoHelpdesk/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import home_view
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
    path('admin/', admin.site.urls),
@@ -24,3 +26,7 @@ urlpatterns = [
    path('support/tickets/', include('tickets.urls'), name='support'),
    path('', home_view, name='home'),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
