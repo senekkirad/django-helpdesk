db439517297a2bc4b9c6381212b52d19cef30162
sénèque HOUNTONDJI <hountondjisenek@gmail.com>
2023-10-16 23:45:55 +0100

    .

--- djangoHelpdesk/settings.py
+++ djangoHelpdesk/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fnr3t-!pygpd3otb3qojxvpedlvv)fa&(25m2+w&s_p*bmjf)!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
