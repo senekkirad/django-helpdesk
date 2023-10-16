08be62e813fd64f57d52163744c8d630a43b0ff2
sénèque HOUNTONDJI <hountondjisenek@gmail.com>
2023-10-16 17:56:26 +0100

    database update to railway

--- djangoHelpdesk/settings.py
+++ djangoHelpdesk/settings.py
@@ -83,13 +83,26 @@ WSGI_APPLICATION = 'djangoHelpdesk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+## For developpement
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#For Vercel +Railway
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER':'postgres',
+        'PASSWORD':'aYpOgGlNqaynHPoXhBel',
+        'HOST':'containers-us-west-45.railway.app',
+        'PORT':'5942',
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
