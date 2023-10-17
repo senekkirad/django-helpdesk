b9403b92b33adce2ff277e5c15a535879157ed37
sénèque HOUNTONDJI <hountondjisenek@gmail.com>
2023-10-17 07:21:26 +0100

    .

--- build_file.sh
+++ build_file.sh
@@ -1,6 +1,23 @@
-# build_files.sh
-pip install -r requirements.txt
+#!/bin/bash
 
-# make migrations
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+# Create a virtual environment
+echo "Creating a virtual environment..."
+python3.9 -m venv venv
+source venv/bin/activate
+
+# Install the latest version of pip
+echo "Installing the latest version of pip..."
+python -m pip install --upgrade pip
+
+# Build the project
+echo "Building the project..."
+python -m pip install -r requirements.txt
+
+# Make migrations
+echo "Making migrations..."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
